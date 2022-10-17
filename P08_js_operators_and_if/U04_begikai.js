0ab4fb4bdaa39fffdaabc8458768697abaaefbd4
MatasVal <valauskasmatas@gmail.com>
2022-10-17 21:57:07 +0300

    0

---
+++ P08_js_operators_and_if/U04_begikai.js
@@ -0,0 +1,14 @@
+let vardas = "Jonas";
+let vieta = 2;
+let medalis = "paguodos";
+
+if (vieta===1)
+{medalis = "auksini"}
+else if(vieta=== 2)
+medalis = "sidabrini";
+else if (vieta === 3) {
+    medalis = "bronzinis";
+}
+
+
+console.log(`${vardas} gavo ${medalis} medali`);
