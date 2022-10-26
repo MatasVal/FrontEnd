d75eef61718be3284be1db81b0982b7f7d6a363e
MatasVal <valauskasmatas@gmail.com>
2022-10-26 21:38:23 +0300

    0

---
+++ P014_js_arrayfunctions/Objects/js/app.js
@@ -0,0 +1,13 @@
+const fitBitData = {
+totalSteps: 308727,
+//totalSteps: 308727,
+totalKMS: 387.7,
+avgCalorieBurn: 400,
+workoutsThisWeek: '5 of 7',
+avgGoodSleep: '2:13',
+45: 'forty five',
+'79 trombones' : 'great song'
+};
+
+console.log(fitBitData.totalSteps);
+//console.log(fitBitData.45);
