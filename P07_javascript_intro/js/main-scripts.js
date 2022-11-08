dfca658da175b4f12b88098419dbf0b2fe476eee
MatasVal <valauskasmatas@gmail.com>
2022-11-08 23:16:06 +0200

    0

--- P07_javascript_intro/js/main-scripts.js
+++ P07_javascript_intro/js/main-scripts.js
@@ -1,3 +1,26 @@
 let color1 = "green";
+let color2 = 'teal';
+let color3 = `yellow`;
 
 console.log("Color1: " + color1);
+console.log("Color2: " + color2);
+console.log("Color2: " + color3);
+
+let numberOne = 10;
+let numberTwo = 80;
+let numberThree = 15.78;
+let numberFour= 15.33333333333333333333333333333333;
+
+console.log(numberOne);
+console.log(numberTwo);
+console.log(numberThree);
+console.log(numberFour);
+
+let no1 = prompt(" enter first number");
+let no2 = prompt(" enter second number");
+
+
+
+
+console.log(no1 + no2); // gaunasi suglaudzia skaicius, konkatinuoja
+console.log(parseInt(no1) + parseInt(no2));
