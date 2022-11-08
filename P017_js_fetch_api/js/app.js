0cc4e5263a3a807ae693f328d10d9696ba13ee5a
MatasVal <valauskasmatas@gmail.com>
2022-11-08 19:37:41 +0200

    0

--- P017_js_fetch_api/js/app.js
+++ P017_js_fetch_api/js/app.js
@@ -1,25 +1,38 @@
 const url = 'https://swapi.dev/api/species'
 
 //const url = 'https://api.meteo.lt/v1/places/vilnius'
+
 const options = {}
 
 const result = {};
 
-fetch(url, options)
-//.then( response => response.json() )
-.then( res => {
-if(res.ok)
-{
-    console.log( res );
-    return res.json();
+const loadDataSimple = () => {
+    fetch(url)
+        //.then( response => response.json() )
+        .then(res => {
+            if (res.ok) {
+                console.log(res);
+                return res.json();
+            }
+            else {
+                console.log("Got error.Status : " + res.status);
+            }
+
+        })
+        .then(data => console.log(data.results[2]));
 }
-else{
-    console.log("Got error.Status : " + res.status);
+
+
+
+const loadDataAsync = async () => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
-    
-}).then(data => console.log(data.results[2]));
-
-
-
-
+loadDataAsync();
+console.log("Mes dabar esam cia");
